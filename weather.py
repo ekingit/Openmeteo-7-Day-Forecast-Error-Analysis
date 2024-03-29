da9424773777916d12596b5d66aec62de0be4d1c
ekingit <164756248+ekingit@users.noreply.github.com>
2024-03-29 10:41:53 +0100

    Update weather.py

--- weather.py
+++ weather.py
@@ -10,8 +10,7 @@ cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
 retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
 openmeteo = openmeteo_requests.Client(session = retry_session)
 
-# Make sure all required weather variables are listed here
-# The order of variables in hourly or daily is important to assign them correctly below
+
 url = "https://api.open-meteo.com/v1/forecast"
 
 # Geopy API, given a city name, produce coordinates
