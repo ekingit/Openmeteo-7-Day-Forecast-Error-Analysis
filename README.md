87216abea2059b04d19ddedfb099d738f9d9be4c
Ekin <ekinkaan@hotmail.com>
2024-03-29 10:26:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a simple practice project for me to learn how to utilize web-APIs and become familiar with Python syntax, Python conventions and GitHub.
+
+I created a function which takes a city name or an address as an input and gives coordinates of the city and produces weather forecasts for the next 7 days.
+
+
+One can use the following command to call the function from the command line:
+python -c "import weather; print(weather.city_name('Vienna'))"
---
+++ weather.py
@@ -0,0 +1,53 @@
+import openmeteo_requests
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+import geopy
+
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://api.open-meteo.com/v1/forecast"
+
+# Geopy API, given a city name, produce coordinates
+geolocator = geopy.geocoders.Nominatim(user_agent="weather_project")
+
+def city_name(var):
+    """Enter a city name or address"""
+    location = geolocator.geocode(var)
+    if location is None:
+        print("the address or city cannot be found. Did you spell city name correctly?")
+        raise SystemExit(0)
+
+    params = {
+	"latitude": location.latitude,
+	"longitude": location.longitude,
+	"daily": ["temperature_2m_max", "temperature_2m_min"]}
+    responses = openmeteo.weather_api(url, params=params)
+    response = responses[0]
+    daily = response.Daily()
+    daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
+    daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
+    daily = response.Daily()
+    daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
+    daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
+    daily_data = {"date": pd.date_range(
+		start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+		end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+		freq = pd.Timedelta(seconds = daily.Interval()),
+		inclusive = "left"
+	)}
+    daily_data["temperature_2m_max"] = daily_temperature_2m_max
+    daily_data["temperature_2m_min"] = daily_temperature_2m_min
+
+    daily_dataframe = pd.DataFrame(data = daily_data)
+    print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+    print(f"Elevation {response.Elevation()} m asl")
+    print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+    return(daily_dataframe)
